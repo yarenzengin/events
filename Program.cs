400a830ce83509e894eff1e81e95811133999846
Yaren <avote841@gmail.com>
2021-03-09 02:41:35 +0300

    events

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product harddisk = new Product(50);
+            harddisk.ProductName = "hard disk";
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;//gsm i bu evente abone ettik
+
+            for (int i = 0; i < 10; i++)
+            {
+                harddisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("gsm about to finish!!");
+        }
+    }
+}
